1173e34e2fd1e4e9347c225aec9f8a890b7005f4
Sayshi <jacobfwai@gmail.com>
2022-04-10 22:41:49 +1200

    Update unitTests.py

--- unitTests.py
+++ unitTests.py
@@ -5,18 +5,22 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.xmlfile = 'Testfiles/testfile.xml'
         with open('Testfiles/12345678901.csv') as f:
-            self.csvfile1 = f.read()
+            self.csvfile1 = ['12345678901', f.read()]
             f.close()
         with open('Testfiles/98765432109.csv') as f:
-            self.csvfile2 = f.read()
+            self.csvfile2 = ['98765432109', f.read()]
             f.close()
 
     def test_matchesOutput(self):
-        self.assertEqual(self.csvfile1, XM().tostring(self.xmlfile, 0))
-        self.assertEqual(self.csvfile2, XM().tostring(self.xmlfile, 1))
+        self.assertEqual(self.csvfile1[1], XM().tostring(self.xmlfile, 0))
+        self.assertEqual(self.csvfile2[1], XM().tostring(self.xmlfile, 1))
 
-    # def test_incorrectStartFormat(self):
-    #     self.assertEqual(True, False)
+    def test_correctNames(self):
+        self.assertEqual(self.csvfile1[0], XM().extractcsvdata(self.xmlfile)[0][0])
+        self.assertEqual(self.csvfile2[0], XM().extractcsvdata(self.xmlfile)[1][0])
+
+    #def testIncorrectFormatting(self):
+    #    self.assertEqual(True, False)
     #
     # def test_incorrectEndFormat(self):
     #     self.assertEqual(True, False)
